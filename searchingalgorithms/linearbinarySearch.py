944a64cd293eb9567fb2b3ce67d5e224f66cdb75
unknown <venkateshk5764@gmail.com>
2025-09-07 23:28:19 +0530

    Today i practiced two algorithms for searching one is linear search and another is binary search

---
+++ searchingalgorithms/linearbinarySearch.py
@@ -0,0 +1,19 @@
+def linearsearch(arr,targetvar):
+    for i in range(0,len(arr)):
+        if(arr[i]==targetvar):
+            print("element found at position ",i)
+def binarysearch(arr,targetvar):
+    arr.sort()
+    lef=0
+    rig=len(arr)-1
+    while lef<=rig:
+        mid=(lef+rig)//2
+        if(arr[mid]==targetvar):
+            return mid
+        elif (arr[mid]<targetvar):
+            lef=mid+1
+        else:
+            rig=mid-1
+    return -1
+result=binarysearch([13,14,16,19,22,24,56],24)
+print(result)
