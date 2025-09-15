09ddbdda8a2f03a5ae4dbbbc06c81198d0f45080
unknown <venkateshk5764@gmail.com>
2025-09-15 22:04:31 +0530

    today started the two pointer approach using lists

---
+++ twopointerproblems/normalreverse.py
@@ -0,0 +1,7 @@
+l=[1,2,3,4,5,6,7]
+l1=[]
+n=len(l)
+for i in range(0,n):
+    l1.append(l[n-1-i])
+print(l1)
+
---
+++ twopointerproblems/positionreverse.py
@@ -0,0 +1,11 @@
+l=[1,2,3,4,5,6,7]
+n=len(l)
+lef=3
+rig=n-1
+while(lef<rig):
+    temp=l[lef]
+    l[lef]=l[rig]
+    l[rig]=temp
+    lef =lef+1
+    rig= rig-1
+print(l)
